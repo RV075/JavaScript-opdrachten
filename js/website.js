5c8ced0d0e19bad6f09d31ac7a26b3893de9b8ba
RV075 <204032@novacollege.nl>
2026-02-20 08:59:57 +0100

    alle onderdelen van opdrachten toegevoegd
    bevat index.html, website.css & website.js
    
    t/m opdr 2.3 gemaakt

---
+++ js/website.js
@@ -0,0 +1,22 @@
+//opdr 1.2
+document.querySelector('#box1').style.backgroundColor = "gold";
+
+//opdr 2.1: on load
+window.addEventListener("load", function () {
+    console.log("De pagina is geladen!");
+});
+
+//opdr 2.2: on click
+document.querySelector('#box1').addEventListener('click', function() {
+    console.log("Ooooh je mag niet klikken!");
+});
+
+//opdr 2.2: on submit
+document.querySelector('#search_form').addEventListener('submit', function() {
+    console.log("Laten we dan nu het hele formulier naar PHP sturen.");
+});
+
+//opdr 2.3
+document.querySelector('#box2').innerText = "Dit is nu de tekst in Box Twee."
+
+document.querySelector('#box2').innerHTML = "<h1>Dit is nu</h1><p>de tekst in Box Twee.</p>";
